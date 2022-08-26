5ef3727cb9cde2054a709c46003a339339716a47
Curtiss Chapman <curtiss.a.chapman@gmail.com>
2022-08-26 15:54:45 -0500

    deleting Mirman orig files and unused files

--- inhkernelCurt.m
+++
@@ -1,8 +0,0 @@
-function actp=inhkernel3(acta)
-%% INHKERNEL3 simgoid inhibition function
-%
-% Input:  acta  - Current activation value
-% Output: actp  - Inhibition value transformed by sigmoid function
-
-actp=15./(1.5+exp(-35.*(acta-0.3)));
-
